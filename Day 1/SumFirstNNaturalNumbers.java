e93a9be283a78e1e19d75b726c1ce51725e6e7a4
MayankSingh2007 <dhruvsingh60hx@gmail.com>
2026-06-02 15:05:21 +0530

    Day 1 Program 1
    Day 1 Program 1 commit.

---
+++ Day 1/SumFirstNNaturalNumbers.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SumFirstNNaturalNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive integer N: ");
+
+        if (scanner.hasNextInt()) {
+            int n = scanner.nextInt();
+            if (n > 0) {
+                int sum = n * (n + 1) / 2;
+                System.out.println("The sum of the first " + n + " natural numbers is: " + sum);
+            } else {
+                System.out.println("Invalid input: N must be a positive integer.");
+            }
+        } else {
+            System.out.println("Invalid input: please enter an integer.");
+        }
+
+        scanner.close();
+    }
+}
