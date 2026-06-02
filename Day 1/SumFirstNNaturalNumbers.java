0298cce113f67d59b75a7442ebbf33732bb8ee44
MayankSingh2007 <dhruvsingh60hx@gmail.com>
2026-06-02 15:07:21 +0530

    Changes in PGM 1
    Changes in PGM 1

--- Day 1/SumFirstNNaturalNumbers.java
+++ Day 1/SumFirstNNaturalNumbers.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class SumFirstNNaturalNumbers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a positive integer N: ");
+        System.out.print("Enter a positive integer Number: ");
 
         if (scanner.hasNextInt()) {
             int n = scanner.nextInt();
